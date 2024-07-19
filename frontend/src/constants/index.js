d121734a6121004cf68521f7a427ca9fd96ab5e7
rakshar26 <57399192+rakshar26@users.noreply.github.com>
2024-07-19 10:09:02 -0500

    Update index.js
    Added AWS EC2 instance endpoint to access Django backend

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000/students";
-export const API_URL_UPDATE = "http://localhost:8000/students/update/";
+export const API_URL = "http://18.217.180.160/students";
+// export const API_URL_UPDATE = "http://localhost:8000/students/update/";
