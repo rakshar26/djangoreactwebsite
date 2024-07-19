0e9e3aa52d977e436c2de2afd7f74ab1fa616088
rakshar26 <57399192+rakshar26@users.noreply.github.com>
2024-07-19 10:08:00 -0500

    Update Header.js
    Added AWS CloudFront domain_name/object_name link of image to be rendered on website

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,13 +4,13 @@ class Header extends Component {
   render() {
     return (
       <div className="text-center">
-        {/* <img
-          src="./3d-cartoon-character.jpg"
-          alt = "pictureCartoon"
+        <img
+          src=src="https://d3mzljgivu8co0.cloudfront.net/1440_SS_marsupial_feat-1030x580.jpg"
+          alt = "kangarooPicture"
           
           className="image"
           
-        /> */}
+        />
         <hr />
        
         <h1>Phone Directory website</h1>
